b705114e64b5f5a2c6ce57fc750b61deccb3a062
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-27 15:55:27 -0300

    Gráfico de receita no período

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from './components/day-orders-amount-card '
 import { MonthCanceledOrdersAmountCard } from './components/month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './components/month-orders-amount-card'
 import { MonthRevenueCard } from './components/month-revenue-card'
+import { RevenueChart } from './components/revenue-chart'
 
 export const Dashboard = () => {
   return (
@@ -17,6 +18,10 @@ export const Dashboard = () => {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
