eac0a42c32516568ec2b033f8504ac00411e7bbf
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-26 23:46:04 -0300

    Configuração do título de cada página com React Helmet Async

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
 export const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
