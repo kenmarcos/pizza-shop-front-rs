534545921e11eff0fd8a96b0739e31ca1f205ac4
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-08-27 11:20:14 -0300

    Usando biblioteca de notificações Toast (Sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,9 +24,20 @@ export const SignIn = () => {
   })
 
   const handleSignIn = async (data: SignInFormData) => {
-    console.log('🚀 ~ handleSignIn ~ data:', data)
+    try {
+      console.log('🚀 ~ handleSignIn ~ data:', data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Renviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
