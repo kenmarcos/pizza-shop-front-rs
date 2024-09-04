46212d300056bc6becbd130a514e866e0c204dbe
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-09-03 22:20:41 -0300

    Implementa exibição de dados do perfil no cabeçalho

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,6 +15,16 @@ import {
 import { Button } from './ui/button'
 
 export const AccountMenu = () => {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -19,16 +32,16 @@ export const AccountMenu = () => {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizzas Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Marcos Kenji</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            marcos.kenji@mail.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
 
