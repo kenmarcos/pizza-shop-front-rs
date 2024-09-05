8dd65d8fe1d905ac376c60db93ea4e57226b8446
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-09-05 14:08:10 -0300

    Adiciona Loading state nos dados do perfil

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,14 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // cookies should be sent from front-end to back-end
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    // antes de todas as requisições do axios, essa função será chamada
+    // config são os dados da requisição (body, headers, etc)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)) // delay de 2 segundos
+
+    return config
+  })
+}
