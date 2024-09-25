fb59cf26e42bd2248ae9e13bcf7afbbd6a12ce63
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-09-25 11:49:18 -0300

    Página de erros e página Not Found

---
+++ src/pages/error.tsx
@@ -0,0 +1,24 @@
+import { Link, useRouteError } from 'react-router-dom'
+
+export const Error = () => {
+  const error = useRouteError() as Error
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Oops, algo deu errado...</h1>
+
+      <p className="text-accent-foreground">
+        Abaixo você encontra mais detalhes
+      </p>
+
+      <pre>{error?.message || JSON.stringify(error)}</pre>
+
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link className="text-sky-600 dark:text-sky-400" to="/">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
